bb759adec7610835e509ecba68995fe92477360d
Nick Cox <nickcox1008@gmail.com>
2021-05-17 23:06:50 -0600

    Untested command parser

--- src/bed-controller.ino
+++ src/bed-controller.ino
@@ -1,28 +1,11 @@
 #include "Particle.h"
 #include "AnalogSmoother.h"
 
-enum STATE {
-	OFF,
-	RAISING,
-	LOWERING
-};
+BedJoint head(A0, 9, 2);
+BedJoint feet(A1, 3, 4);
 
-struct BED {
-	int potPin;
-	int upPin;
-	int downPin;
-	int targetAngle;
-	int maxAngle;
-	int mapLow;
-	int mapHigh;
-	float lastReading;
-	float tolerance;
-	STATE state;
-	AnalogSmoother pot;
-};
-
-struct BED head = {A0, 9, 2, 0, 50, 0, 270, 0, 5, OFF, AnalogSmoother(A0, 10)};
-struct BED feet = {A1, 3, 4, 0, 45, 0, 270, 0, 5, OFF, AnalogSmoother(A1, 10)};
+int ANGLE_UP = 10;
+int ANGLE_TV = 10;
 
 void setup() {
 	Particle.function("moveHead", moveHead);
@@ -36,145 +19,118 @@ void setup() {
 
 	Serial.begin(115200);
 
-	pinMode(head.potPin,  INPUT);
-	pinMode(feet.potPin,  INPUT);
-	pinMode(head.upPin,   OUTPUT);
-	pinMode(feet.upPin,   OUTPUT);
-	pinMode(head.downPin, OUTPUT);
-	pinMode(feet.downPin, OUTPUT);
-
-	head.pot.fill();
-	feet.pot.fill();
-
-	head.lastReading = head.pot.read();
-	feet.lastReading = feet.pot.read();
+	head.init();
+	feet.init();
 
 	RGB.control(true);
 	RGB.brightness(0); // 0 - 255
 	RGB.control(false);
 }
 
-int getAngle(float value, int mapLow, int mapHigh) {
-	return map((int)round(value), 0, 1024, mapLow, mapHigh);
-}
-int readAngle(BED &bed) {
-	return getAngle(bed.pot.read(), bed.mapLow, bed.mapHigh);
-}
-
-void turnOff(BED &bed) {
-	bed.state = OFF;
-	// Turn switches off
-	digitalWrite(bed.upPin, LOW);
-	digitalWrite(bed.downPin, LOW);
-}
-
-void checkAngle(BED &bed) {
-	if (bed.state == OFF) {
-		return;
-	}
-
-	float value = bed.pot.read();
-	int angle = getAngle(value, bed.mapLow, bed.mapHigh);
-
-	if (bed.state == RAISING) {
-		// If the bed has reached it's target
-		if (angle >= bed.targetAngle) {
-			turnOff(bed);
-		}
-		// If the bed hasn't moved since last time
-		if (value <= bed.lastReading + bed.tolerance) {
-			//turnOff(bed);
-		}
-	}
-	else if (bed.state == LOWERING) {
-		// If the bed has reached it's target
-		if (angle <= bed.targetAngle) {
-			turnOff(bed);
-		}
-		// If the bed hasn't moved since last time
-		if (value >= bed.lastReading - bed.tolerance) {
-			//turnOff(bed);
-		}
-	}
-
-	bed.lastReading = value;
-}
-
 void loop() {
-	checkAngle(head);
-	checkAngle(feet);
+	head.update();
+	feet.update();
 }
 
-int setAngle(String angle, BED &bed) {
-	Serial.printlnf("%d Set angle %s", (int)Time.now(), angle.c_str());
-
-	turnOff(bed);
-
-	bed.targetAngle = angle.toInt();
-	int currentAngle = readAngle(bed);
-
-	if (bed.targetAngle < 0) {
-		bed.targetAngle = 0;
-	}
-	else if (bed.targetAngle > bed.maxAngle) {
-		bed.targetAngle = bed.maxAngle;
-	}
-
-	if (bed.targetAngle > currentAngle) {
-		bed.state = RAISING;
-		// Turn on the up switch
-		digitalWrite(bed.upPin, HIGH);
-	}
-	else if (bed.targetAngle < currentAngle) {
-		bed.state = LOWERING;
-		// Turn on the down switch
-		digitalWrite(bed.downPin, HIGH);
-	}
-
-	return bed.state;
-}
 int setHeadAngle(String angle) {
-	return setAngle(angle, head);
+	return head.setAngle(angle.toInt());
 }
 int setFeetAngle(String angle) {
-	return setAngle(angle, feet);
+	return feet.setAngle(angle.toInt());
 }
 
 int getHeadAngle() {
-	return readAngle(head);
+	return head.currentAngle();
 }
 int getFeetAngle() {
-	return readAngle(feet);
+	return feet.currentAngle();
 }
 
-int setState(String state, BED &bed) {
-	state.toLowerCase();
-
-	if (state == "OFF") {
-		bed.state = OFF;
-		digitalWrite(bed.upPin, LOW);
-		digitalWrite(bed.downPin, LOW);
-	}
-	else if (state == "raise") {
-		bed.state = RAISING;
-		digitalWrite(bed.upPin, HIGH); // Turn on the up switch
-		digitalWrite(bed.downPin, LOW);
-	}
-	else if (state == "lower") {
-		bed.state = LOWERING;
-		digitalWrite(bed.upPin, LOW);
-		digitalWrite(bed.downPin, HIGH); // Turn on the down switch
-	}
-
-	return bed.state;
-}
 int setHeadState(String state) {
 	Serial.printlnf("%d Set head state %s", (int)Time.now(), state.c_str());
-	return setState(state, head);
+	return head.setState(state);
 }
 int setFeetState(String state) {
 	Serial.printlnf("%d Set feet state %s", (int)Time.now(), state.c_str());
-	return setState(state, feet);
+	return feet.setState(state);
+}
+
+int move(String cmd, BedJoint &bed) {
+	// Command starters:
+	/*
+	Move my bed...
+	Lay my bed...
+	Sit my bed...
+	*/
+
+	// Commands:
+	/*
+	to X degrees
+	to [named position]
+	up
+	up X degrees
+	up by X degrees
+	down
+	down X degrees
+	down by X degrees
+	*/
+
+	// REGEX: /^(up|down|to)\s*(by)?\s*(\d+)?\s*()?$/
+
+	cmd.trim().toLowerCase();
+
+	bool endsWithDegrees = cmd.endsWith("degrees");
+	if (endsWithDegrees) {
+		cmd = cmd.substring(0, cmd.length()-7).trim();
+	}
+
+	if (cmd.startsWith("to")) {
+		cmd = cmd.substring(2).trim();
+
+		if (endsWithDegrees) {
+			return bed.setAngle(cmd.toInt());
+		}
+		else if (cmd == "tv") {
+			return bed.setAngle(ANGLE_TV);
+		}
+	}
+	else if (cmd.startsWith("up")) {
+		cmd = cmd.substring(2).trim();
+
+		if (cmd.startsWith("by")) {
+			cmd = cmd.substring(2).trim();
+		}
+
+		if (cmd == "") {
+			return bed.setAngle(ANGLE_UP);
+		}
+		else {
+			return bed.addAngle(cmd.toInt());
+		}
+	}
+	else if (cmd.startsWith("down")) {
+		cmd = cmd.substring(4).trim();
+
+		if (cmd.startsWith("by")) {
+			cmd = cmd.substring(2).trim();
+		}
+
+		if (cmd == "") {
+			return bed.setAngle(0);
+		}
+		else {
+			return bed.addAngle(-cmd.toInt());
+		}
+	}
+
+	Serial.printlnf("%d Error parsing command: '%s'", (int)Time.now(), cmd.c_str());
+	return bed.setAngle(0);
+}
+int moveHead(String cmd) {
+	return move(cmd, head);
+}
+int moveFeet(String cmd) {
+	return move(cmd, feet);
 }
 
 void publish(String event, String data) {
