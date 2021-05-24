67a8cb1462ea604fc5ca34dec8f6b47cdeb8df60
Nick Cox <nickcox1008@gmail.com>
2021-05-24 01:05:48 -0600

    Helper / reminder particle command scripts

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+#command command destination dirctory
+#                  device
+particle compile    argon    src   --saveTo firmware.bin
---
+++ flash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+#echo 'Make sure your device is connected via USB and flashing yellow.'
+#read -r -p 'Do you want to continue? [Y/n] ' key
+
+#key=${key,,} # to lower
+
+#if [ "$key" = 'n' ]; then
+#	exit
+#fi
+
+particle flash --usb firmware.bin
