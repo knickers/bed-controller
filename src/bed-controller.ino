a2d95694970a85e5d2ff73720c28daf3ebbd3543
Nick Cox <nickcox1008@gmail.com>
2021-07-04 23:20:57 -0600

    Rename error event

--- src/bed-controller.ino
+++ src/bed-controller.ino
@@ -74,7 +74,7 @@ State* _pos = parser.addState([]() {
 State* _err = parser.addState([]() {
 	head.turnOff();
 	feet.turnOff();
-	publish("error", "Invalid named position: '" + token.val() + "'");
+	publish("bed/error", "Invalid named position: '" + token.val() + "'");
 });
 
 
